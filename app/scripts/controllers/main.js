0ddb22537c5ccd70e12f37d6b0bfa8045770aa9b
I3rendan <brendan@brendanenglish.com>
2015-12-21 23:59:15 -0500

    updated slides

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('socialwallApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout, $interval) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
 
 
     // Demo Stuff
@@ -22,11 +23,22 @@ angular.module('socialwallApp')
     // End Demo
 
 
+    var $container = angular.element('#masonry-wrap');
+
+    $scope.getRandomArbitrary = function(min, max){
+      return Math.round(Math.random() * (max - min) + min);
+    };
+
+    $interval(function(){
+      $scope.makeActive($scope.getRandomArbitrary(0, $scope.bricks.length));
+    }, 3000);
+
     $scope.makeActive = function(index){
       $scope.isActive = index;
-
-      angular.element('#masonry-wrap').masonry('reloadItems');
-      angular.element('#masonry-wrap').masonry();
+      $timeout(function(){
+        $container.masonry('reloadItems');
+        $container.masonry('layout');
+      }, 500);
     };
 
     $scope.bricks = [
