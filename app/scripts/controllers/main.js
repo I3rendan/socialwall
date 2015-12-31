261565c4ad8401acf661f1b29a35fc969e95d56c
I3rendan <brendan@brendanenglish.com>
2015-12-31 18:18:21 -0500

    updated

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('socialwallApp')
-.controller('MainCtrl', function ($scope, $http, $timeout) {
+.controller('MainCtrl', function ($scope, $http, $timeout, $window) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
@@ -9,75 +9,148 @@ angular.module('socialwallApp')
   ];
 
   $scope.bricks = [];
+  $scope.bricksDupe = [];
   $scope.data = [];
-  $scope.delayTime = 3000;
+  $scope.gridCount = 0;
+  $scope.newLeft = 0;
+  $scope.newTop = 0;
+  $scope.colNum = 5;
+  $scope.loopTime = 3000;
+  $scope.gifRunTime = 2000;
   $scope.container = angular.element('#masonry-wrap');
 
-  function getPhotos(){
+  $scope.getPhotos = function(){
 
     // The offical feed url...
-    $http.get('/app/montagephotos/cfp').success(function(data){
-    //$http.get('/db/feed.json').success(function(data){
+    //$http.get('/app/montagephotos/cfp').success(function(data){
+    $http.get('/db/feed.json').success(function(data){
       $scope.bricks.push(data.photos);
+      $scope.bricksDupe = $scope.bricks;
       $scope.isActive = -1;
-      $timeout(function(){
-        shuffleBricks();
-      }, 750);
+      $scope.loopTime = data.loopTime * 1000;
+      $scope.gifRunTime = data.gifRunTime * 1000;
+      $scope.colNum = data.colNum;
+      $scope.sizeGrid();
     });
-    $scope.loopTimeout = $timeout(getNewPhotos, $scope.delayTime);
-  }
+    $scope.loopTimeout = $timeout(function(){
+      $scope.getNewPhotos();
+    }, $scope.loopTime);
+  };
 
-  getPhotos();
+  $scope.getPhotos();
 
-  function getNewPhotos(){
+  $scope.sizeGrid = function(){
+    $scope.colNumWidth = 100 / $scope.colNum;
+    $scope.winWidth = $window.innerWidth;
+    $scope.winHeight = $window.innerHeight;
+    $scope.brickWidth = angular.element('.brick').width();
+    $scope.brickHeight = angular.element('.brick').height();
+    $scope.gridCount = Math.round($scope.winWidth / $scope.brickWidth) * Math.round($scope.winHeight / $scope.brickHeight);
+  };
+
+  $scope.sizeNewBrick = function(){
+    $scope.newBrickWidth = angular.element('#new-brick').width();
+    $scope.newBrickHeight = angular.element('#new-brick').height();
+
+    console.log('NEW WIDTH: ' + $scope.newBrickWidth);
+    console.log('NEW HEIGHT: ' + $scope.newBrickHeight);
+    
+    $scope.newLeft = ($scope.winWidth / 2) - ($scope.newBrickWidth / 2) + 'px';
+    $scope.newTop = ($scope.winHeight / 2) - ($scope.newBrickHeight / 2) + 'px';
+    $scope.newBrickStyle = {'top': $scope.newTop, 'left': $scope.newLeft};
+  };
+
+  angular.element($window).bind('resize', function(){
+    $scope.sizeGrid();
+  });
+
+  $scope.getNewPhotos = function(){
 
     // The offical feed url...
-    $http.get('/app/montagephotosnew/cfp').success(function(data){
-    //$http.get('/db/newFeed.json').success(function(data){
+    //$http.get('/app/montagephotosnew/cfp').success(function(data){
+    $http.get('/db/newFeed.json').success(function(data){
 
       $scope.data = [];
       $scope.data.push(data.photos);
-
-      // THey don't have this on the json yet so commenting out...
-      // $scope.delayTime = data.loopTime * 1000;
+      $scope.loopTime = data.loopTime * 1000;
+      $scope.gifRunTime = data.gifRunTime * 1000;
 
       if (data.result === 0){
+        $timeout.cancel($scope.gifTimeout);
         $timeout.cancel($scope.loopTimeout);
-        $scope.isActive = getRandomArbitrary(0, $scope.bricks[0].length - 1);
-        $timeout(function(){
-          shuffleBricks();
-          $scope.loopTimeout = $timeout(getNewPhotos, $scope.delayTime);
-        }, 750);
+        $scope.swapContent(false);
+        $scope.loopTimeout = $timeout(function(){
+          $scope.getNewPhotos();
+        }, $scope.loopTime);
       } else {
-        addNewLoop();
+        $scope.addNewLoop();
       }
     });
-  }
+  };
 
-  function addNew(){
-    if ($scope.data[0].length > 0){
-      $scope.bricks[0].pop();
-      $scope.bricks[0].unshift($scope.data[0][0]);
-      $scope.data[0].shift();
-      $scope.isActive = 0;
-      $timeout(function(){
-        shuffleBricks();
-        $scope.loopTimeout = $timeout(addNewLoop, $scope.delayTime);
-      }, 750);
+  $scope.swapContent = function(isNew){
+
+    $scope.isActive = getRandomArbitrary(0, $scope.bricks[0].length - 1);
+    $scope.swapItem = angular.element('#brick' + $scope.isActive);
+
+    if (isNew === true){
+
+      $scope.newBrick = $scope.data[0][0];
+      $scope.newLoad = true;
+      $scope.doneLoad = false;
+      $scope.sizeNewBrick();
+
+      $scope.gifTimeout = $timeout(function(){
+        $scope.newLoad = false;
+        $scope.doneLoad = true;
+
+        $scope.swapItem.animate({ opacity: 0 }, 500, function(){
+          $scope.bricks[0][$scope.isActive] = $scope.newBrick;
+          $scope.data[0].shift();
+          $scope.bricksDupe = $scope.bricks;
+          $scope.$apply();
+          $scope.swapItem.animate({ opacity: 1 }, 500);
+        });
+
+        var swapTop = $scope.swapItem.offset().top;
+        var swapLeft = $scope.swapItem.offset().left;
+        var swapWidth = $scope.swapItem.width();
+        var swapHeight = $scope.swapItem.height();
+
+        $scope.newTop = swapTop + (swapHeight / 2) + 'px';
+        $scope.newLeft = swapLeft + (swapWidth / 2) + 'px';
+        $scope.newBrickStyle = {'top': $scope.newTop, 'left': $scope.newLeft};
+        
+        
+        $scope.loopTimeout = $timeout(function(){
+          $scope.addNewLoop();
+        }, $scope.loopTime);
+
+      
+      }, $scope.gifRunTime);
     } else {
-      $scope.loopTimeout = $timeout(getNewPhotos, $scope.delayTime);
+      $scope.swapItem.animate({ opacity: 0 }, 500, function(){
+        $scope.newBrick = $scope.bricksDupe[0][getRandomArbitrary(0, $scope.bricks[0].length - 1)];
+        $scope.bricks[0][$scope.isActive] = $scope.newBrick;
+        $scope.$apply();
+        $scope.swapItem.animate({ opacity: 1 }, 500);
+      });
     }
-  }
+  };
 
-  function addNewLoop(){
+  $scope.addNew = function(){
+    if ($scope.data[0].length > 0){
+      $scope.swapContent(true);
+    } else {
+      $scope.getNewPhotos();
+    }
+  };
+
+  $scope.addNewLoop = function(){
+    $timeout.cancel($scope.gifTimeout);
     $timeout.cancel($scope.loopTimeout);
-    addNew();
-  }
-
-  function shuffleBricks(){
-    $scope.container.masonry('reloadItems');
-    $scope.container.masonry('layout');
-  }
+    $scope.addNew();
+  };
 
   function getRandomArbitrary(min, max){
     return Math.round(Math.random() * (max - min) + min);
